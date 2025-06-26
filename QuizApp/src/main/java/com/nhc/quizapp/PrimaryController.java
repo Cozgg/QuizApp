1d65c9a85dd19dd84958ece8231318a33d4e1ecb
Cozgg <nguyenhuucong295@gmail.com>
2025-06-26 16:24:14 +0700

    last update 26/6/2025

--- QuizApp/src/main/java/com/nhc/quizapp/PrimaryController.java
+++ QuizApp/src/main/java/com/nhc/quizapp/PrimaryController.java
@@ -1,16 +1,16 @@
 package com.nhc.quizapp;
 
 import com.nhc.utils.MyAlert;
+import com.nhc.utils.MyStage;
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    public void handleQuestionManagement(ActionEvent event){
-        MyAlert.getInstance().getMsg("Cozg dep trai chua mo");
-        
+    public void handleQuestionManagement(ActionEvent event) throws IOException {
+          MyStage.getInstance().showStage("question.fxml");
     }
+
     public void handlePracticeButton(ActionEvent event){
         MyAlert.getInstance().getMsg("Coming soon...");
     }
--- QuizApp/src/main/java/module-info.java
+++ QuizApp/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.nhc.quizapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.nhc.quizapp to javafx.fxml;
     exports com.nhc.quizapp;
