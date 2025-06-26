4cf836e7f53259facc76ccf673d0ec6ae89ebb22
Cozgg <nguyenhuucong295@gmail.com>
2025-06-26 14:39:39 +0700

    Update

--- QuizApp/src/main/java/com/nhc/quizapp/PrimaryController.java
+++ QuizApp/src/main/java/com/nhc/quizapp/PrimaryController.java
@@ -1,12 +1,17 @@
 package com.nhc.quizapp;
 
+import com.nhc.utils.MyAlert;
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void handleQuestionManagement(ActionEvent event){
+        MyAlert.getInstance().getMsg("Cozg dep trai chua mo");
+        
+    }
+    public void handlePracticeButton(ActionEvent event){
+        MyAlert.getInstance().getMsg("Coming soon...");
     }
 }
